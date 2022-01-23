152aafe6f10a18bfa2dcc4190027f4497727a6bc
Hisam Fahri <hisam@alphaaiauto.com>
2022-01-23 15:15:18 +0700

    feat(authorized root): use jwt middleware

--- cmd/main.go
+++ cmd/main.go
@@ -5,19 +5,25 @@ import (
 	"github.com/hisamafahri/rest-go/internal/auth"
 	"github.com/hisamafahri/rest-go/internal/root"
 	"github.com/hisamafahri/rest-go/internal/teacher"
+	"github.com/hisamafahri/rest-go/pkg/authToken"
 	"github.com/hisamafahri/rest-go/pkg/db"
 )
 
 func main() {
 	db.DB()
 	route := gin.Default()
-	route.GET("/", root.GetRoot)
 
-	route.GET("/teacher", teacher.GetAllTeacher)
-	route.GET("/teacher/:id", teacher.GetOneTeacher)
-	route.DELETE("/teacher/:id", teacher.DeleteOneTeacher)
-	route.POST("/teacher", teacher.CreateOneTeacher)
-	route.PUT("/teacher", teacher.UpdateOneTeacher)
+	authorized := route.Group("/")
+
+	authorized.Use(authToken.VerifyToken())
+	{
+		authorized.GET("/", root.GetRoot)
+		authorized.GET("/teacher", teacher.GetAllTeacher)
+		authorized.GET("/teacher/:id", teacher.GetOneTeacher)
+		authorized.DELETE("/teacher/:id", teacher.DeleteOneTeacher)
+		authorized.POST("/teacher", teacher.CreateOneTeacher)
+		authorized.PUT("/teacher", teacher.UpdateOneTeacher)
+	}
 
 	route.GET("/auth", auth.GenerateToken)
 	route.Run(":3000")
